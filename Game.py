78e0cbc23420e9a17aab3c0f06ad97111bc5d760
Rob Bird <robertbirdwv@gmail.com>
2021-10-11 11:42:47 -0400

    Moved rollers into new file. Added loop for main.

--- Game.py
+++ Game.py
@@ -1,61 +1,34 @@
-import random
+from MyUtilities import dieRoller, targetRoller
 
 
-def dieRoller(quantityOfDice, numberOfSides):
-    # print('In dieRoller: ')
-    # if numberOfSides <= 1:
-    #     print('Cannot have one-sided die.')
-    #     return 0
+def main():
+    while True:
 
-    allRolls = []
+        quantity = int(input('How many times to roll (0 to quit): '))
 
-    for x in range(0, quantityOfDice):
-        roll = int(random.randint(1, numberOfSides))
-        # print('Roll: ' + str(roll))
-        allRolls.append(roll)
+        if quantity == 0:
+            quit()
 
-    # print('AllRolls: ' + str(allRolls))
-    return allRolls
+        sides = int(input('Sides per die: '))
+
+        if sides <= 1:
+            print('Cannot have a one-sided die.')
+            continue
+
+        target = int(input('Target to beat: '))
+
+        rolls = dieRoller(quantity, sides)
+
+        print("Your rolls: ")
+
+        for x in rolls:
+            print(x, end=' ')
+
+        print()
+        if target > 0:
+            # rolls = dieRoller(quantity, sides)
+            successes = targetRoller(rolls, target)
+            print('Number of successes: ' + str(successes))
 
 
-def targetRoller(allRolls, targetNumber):
-    # print('in target roller:')
-
-    numPassed = 0
-
-    for x in allRolls:
-        # print('x is: ' + str(x))
-        if x >= targetNumber:
-            numPassed += 1
-
-    # print('num passed : ' + str(numPassed))
-    return numPassed
-
-
-while True:
-
-    quantity = int(input('How many times to roll (0 to quit): '))
-
-    if quantity == 0:
-        quit()
-
-    sides = int(input('Sides per die: '))
-
-    if sides <= 1:
-        print('Cannot have a one-sided die.')
-        continue
-
-    target = int(input('Target to beat: '))
-
-    rolls = dieRoller(quantity, sides)
-
-    print("Your rolls: ")
-
-    for x in rolls:
-        print(x, end=' ')
-
-    print()
-    if target > 0:
-        # rolls = dieRoller(quantity, sides)
-        successes = targetRoller(rolls, target)
-        print('Number of successes: ' + str(successes))
+main()
