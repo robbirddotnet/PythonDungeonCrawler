d7864ba701aa5a316c0764116dacd272d1c87eb7
Rob Bird <robertbirdwv@gmail.com>
2021-12-06 11:29:22 -0500

    Enemy name list.

--- Combat.py
+++ Combat.py
@@ -1,7 +1,31 @@
 from MyUtilities import dieRoller, clearScreen
 from PlayerGenerator import generateCharacter
+import random
+
+enemyNameList = [
+    "Orc",
+    "Goblin",
+    "Barbarian",
+    "Gnoll",
+    "Elf",
+    "Bear",
+    "Imp",
+    "Zombie",
+    "Kobold",
+    "Giant Spider",
+    "Reptile of Enormous Proportions",
+    "Giant Rat",
+    "Giant Bat",
+    "Internet Troll",
+    "Devil",
+    "Demon",
+    "Evil Genie",
+    "Slime",
+    "Beholder"
+]
 
 
 def createEnemy(player):
     enemy = generateCharacter(player["level"])
+    enemy["name"] = random.choice(enemyNameList)
     return enemy
