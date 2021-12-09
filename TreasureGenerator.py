0003c9051df607bc2369809d659c0dae352efa38
Rob Bird <robertbirdwv@gmail.com>
2021-12-09 12:17:17 -0500

    Treasure lists made for random item names.

---
+++ TreasureGenerator.py
@@ -0,0 +1,33 @@
+from MyUtilities import dieRoller
+import random
+
+# lists of stuff
+treasureAttribute = [
+    'Power',
+    'Greatness',
+    'Badassery',
+    'the Internet',
+    'Silliness',
+    'Strength',
+    'Vitality',
+    'Vile Darkness',
+    'Things and Stuff',
+    'Madness',
+    'Positive Vibes',
+    'Rainbows'
+]
+
+treasureObject = [
+    'Amulet',
+    'Staff',
+    'Ring',
+    'Necklace',
+    'Token',
+    'Badge',
+    'Cape',
+    'Sandals',
+    'Loincloth',
+    'Crown',
+    'Goblet',
+    'Book'
+]
