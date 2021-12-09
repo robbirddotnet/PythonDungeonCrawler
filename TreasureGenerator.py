f3e882e9b76643a964d9c7cd29b67b0b884d704c
Rob Bird <robertbirdwv@gmail.com>
2021-12-09 13:23:36 -0500

    Add minor treasures.

--- TreasureGenerator.py
+++ TreasureGenerator.py
@@ -31,3 +31,30 @@ treasureObject = [
     'Goblet',
     'Book'
 ]
+minorSetup = [
+    'You find ',
+    'You meet ',
+    'You are discovered by '
+    'You come upon '
+]
+minorNoun = [
+    'a Dragon',
+    'a Monk',
+    'another lost soul',
+    'a swarm of rats',
+    'an Elven archer',
+    'a wise old warrior',
+    'an ancient spirit'
+]
+
+
+def minorTreasure(player):
+    type = random.choice(["Health", "Attack", "Defense"])
+    if type == 'health':
+        treasureValue = sum(dieRoller(2, 4))
+    else:
+        treasureValue = dieRoller(1, 4)[0]
+
+    print(str(random.choice(minorSetup)) + str(random.choice(minorNoun)) + '.')
+
+    print("+" + str(treasureValue) + " " + str(type))
