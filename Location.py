c79376d54731bb10430ef71b07009a4651616e80
Rob Bird <robertbirdwv@gmail.com>
2022-01-27 13:36:13 -0500

    Start of Locations

---
+++ Location.py
@@ -0,0 +1,21 @@
+from MyUtilities import *
+
+class Location():
+    def __init__(self):
+        self.manager = ""
+        self.forecolor = "white"
+        self.backcolor = "black"
+        self.week = 1
+
+# term = Terminal()
+# with term.location(0, term.height - 1):
+#     # print(getattr(term, "red_on_blue")+'ALL SYSTEMS GO')
+#     # print(getattr(term, "snow_on_blue")+'ALL SYSTEMS GO')
+#     # print(getattr(term, "snow_on_blue2")+'ALL SYSTEMS GO')
+#     # print(getattr(term, "snow_on_blue3")+'ALL SYSTEMS GO')
+#     # print(term.red('ALL SYSTEMS GO'))
+#     # print(term.normal)
+
+#     print(getattr(term, "snow_on_blue3"))
+#     print(c1)
+#     print(term.normal)
