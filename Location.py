929155be251a5efc09de06f7f9c36823c1976f28
Rob Bird <robertbirdwv@gmail.com>
2022-01-27 14:09:40 -0500

    Location class inheritance

--- Location.py
+++ Location.py
@@ -19,6 +19,35 @@ class Location():
         # called when time advances
         self.week += 1
 
+
+class Stables(Location):
+    def __init__(self, player):
+        super().__init__()
+        self.forecolor = "brown"
+
+
+class Town(Location):
+    def __init__(self, player):
+        super().__init__()
+        self.forecolor = "yellow"
+
+
+class Lumbermill(Location):
+    def __init__(self, player):
+        super().__init__()
+        self.forecolor = "orange"
+
+
+class Palace(Location):
+    def __init__(self, player):
+        super().__init__()
+        self.forecolor = "gold"
+        self.gold_donated = 0
+
+    def donate_gold(player, value):
+        player.gold -= value
+        gold_donated += value
+
 # term = Terminal()
 # with term.location(0, term.height - 1):
 #     # print(getattr(term, "red_on_blue")+'ALL SYSTEMS GO')
